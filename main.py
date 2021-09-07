22cfddf33d22b8515f86c994348638c129f83cb4
Steve Mullen <starchild87@sbcglobal.net>
2021-09-07 09:45:20 -0500

    updates

--- main.py
+++ main.py
@@ -18,4 +18,18 @@ async def index(request: Request):
 @app.post("/process", response_class=HTMLResponse)
 async def process(request: Request,
                   lines: str = Form(...)):
+    print(lines)
+    print(len(lines))
+    items = lines.split("\t")
+    print(len(items))
+    num = ''
+    ndx = 0
+    for item in items:
+        if ndx % 3 == 0:
+            for c in item:
+                if c.isdigit():
+                    num += str(c)
+            print(num)
+            num = ''
+        ndx += 1
     return templates.TemplateResponse("index.html", {"request": request, "lines": lines})
